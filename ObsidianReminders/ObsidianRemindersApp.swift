c2e8c34b7a580ef233274087ca0e4c43ca79b036
Yulian Itskov-Curto <yulian.zigrino@gmail.com>
2026-05-25 21:34:29 -0400

    Initial Commit

---
+++ ObsidianReminders/ObsidianRemindersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ObsidianRemindersApp.swift
+//  ObsidianReminders
+//
+//  Created by Yulian Itskov-Curto on 5/25/26.
+//
+
+import SwiftUI
+
+@main
+struct ObsidianRemindersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
